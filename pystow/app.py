b2ecd258247220499a25a3657035aab00bc9480a
Maciej Barć <xgqt@protonmail.com>
2020-07-19 00:04:27 +0200

    link and unlink working

--- pystow/app.py
+++ pystow/app.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
 
-"""
--d DIR, --dir=DIR     Set stow dir to DIR (default is current dir)
--t DIR, --target=DIR  Set target to DIR (default is parent of stow dir)
--S, --stow            Stow the package names that follow this option
--D, --delete          Unstow the package names that follow this option
--R, --restow          Restow (like stow -D followed by stow -S)
--n, --no, --simulate  Do not actually make any filesystem changes
--V, --version         Show stow version number
--h, --help            Show this help
-"""
-
-
 import os
 import sys
 import getopt
@@ -24,7 +12,6 @@ def unlink(f):
     else:
         print(f, "is not a link")
 
-# cur. directory - os.getcwd()
 
 def stow(directory=".", target="..", action="stow", simulate=False, verbose=False):
 
@@ -38,56 +25,81 @@ def stow(directory=".", target="..", action="stow", simulate=False, verbose=Fals
             "\n"
         )
 
-        def linker(d, t):
-            for inside in os.listdir(d):
-                if os.path.isfile(t + "/" + inside) or os.path.islink(t + "/" + inside):
-                    print("File", t + "/" + inside, "already exists")
-                elif os.path.isdir(t + "/" + inside):
-                    linker(d + "/" + inside, t + "/" + inside)
-                else:
-                    if simulate:
-                        print("Will link " + d + "/" + inside + " to " + t + "/")
-                    else:
-                        os.symlink(
-                            os.path.abspath(d + "/" + inside),
-                            t + "/" + inside
-                        )
-
-        if action == "stow":
-            if directory == ".":
-                for inside in os.listdir(directory):
-                    if os.path.isdir(inside) and not inside[0] == ".":
-                        linker(inside, target)
+    def linker(d, t):
+        for i in os.listdir(d):
+            if verbose:
+                print(d + "/" + i, t + "/" + i)
+            if os.path.isfile(t + "/" + i) or os.path.islink(t + "/" + i):
+                print("File", t + "/" + i, "already exists")
+            elif os.path.isdir(t + "/" + i):
+                linker(d + "/" + i, t + "/" + i)
             else:
-                linker(directory, target)
-        elif action == "unstow":
-            """
-            for ...
-                check link
-            if link == as here
-                unlink
-            """
-            pass
+                if simulate:
+                    print("Will link " + d + "/" + i + " to " + t + "/" + i)
+                else:
+                    os.symlink(
+                        os.path.abspath(d + "/" + i),
+                        t + "/" + i
+                    )
 
-        elif action == "restow":
-            stow(
-                directory=directory,
-                target=target,
-                action="unstow",
-                simulate=simulate,
-                verbose=verbose
-            )
-            stow(
-                directory=directory,
-                target=target,
-                action="stow",
-                simulate=simulate,
-                verbose=verbose
-            )
+    def unlinker(d, t):
+        for i in os.listdir(d):
+            if verbose:
+                print(d + "/" + i, t + "/" + i)
+            if os.path.islink(t + "/" + i):
+                if simulate:
+                    print("Will remove " + t + "/" + i)
+                else:
+                    os.unlink(t + "/" + i)
+            elif os.path.isfile(t + "/" + i):
+                print("File", t + "/" + i, "already exists")
+            elif os.path.isdir(t + "/" + i):
+                unlinker(d + "/" + i, t + "/" + i)
+
+    if action == "stow":
+        if directory == ".":
+            for inside in os.listdir(directory):
+                if os.path.isdir(inside) and not inside[0] == ".":
+                    linker(inside, target)
+        else:
+            linker(directory, target)
+    elif action == "unstow":
+        if directory == ".":
+            for inside in os.listdir(directory):
+                if os.path.isdir(inside) and not inside[0] == ".":
+                    unlinker(inside, target)
+        else:
+            unlinker(directory, target)
+    elif action == "restow":
+        stow(
+            directory=directory,
+            target=target,
+            action="unstow",
+            simulate=simulate,
+            verbose=verbose
+        )
+        stow(
+            directory=directory,
+            target=target,
+            action="stow",
+            simulate=simulate,
+            verbose=verbose
+        )
 
 
 def print_help():
-    print("Help")
+    print(
+        """
+-d DIR, --dir=DIR     Set stow dir to DIR (default is current dir)
+-t DIR, --target=DIR  Set target to DIR (default is parent of stow dir)
+-S, --stow            Stow the package names that follow this option
+-D, --delete          Unstow the package names that follow this option
+-R, --restow          Restow (like stow -D followed by stow -S)
+-n, --no, --simulate  Do not actually make any filesystem changes
+-V, --version         Show stow version number
+-h, --help            Show this help
+"""
+    )
 
 
 def main(argv):
@@ -95,7 +107,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(
             argv,
-            "d:t:DRShnv",
+            "d:t:DRShnVv",
             [
                 "dir=",
                 "target=",
@@ -105,7 +117,8 @@ def main(argv):
                 "help"
                 "no",
                 "simulate",
-                "version"
+                "version",
+                "verbose"
             ]
         )
     except getopt.GetoptError:
