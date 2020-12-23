5e867f8337fb7e2accb881327f0522d121696bd5
Maciej Barć <xgqt@protonmail.com>
2020-12-23 21:58:30 +0100

    setup.py: bump to 1.2.1
    Signed-off-by: Maciej Barć <xgqt@protonmail.com>

--- setup.py
+++ setup.py
@@ -5,35 +5,35 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.md', encoding='utf-8') as f:
+    with open("README.md", encoding="utf-8") as f:
         return f.read()
 
 
 setup(
-    name='pystow',
-    version='1.2.0',
-    description='GNU Stow rewritten in Python',
-    author='XGQT',
-    author_email='xgqt@protonmail.com',
-    url='https://gitlab.com/xgqt/pystow',
+    name="pystow",
+    version="1.2.1",
+    description="GNU Stow rewritten in Python",
+    author="XGQT",
+    author_email="xgqt@protonmail.com",
+    url="https://gitlab.com/xgqt/pystow",
     long_description=readme(),
-    long_description_content_type='text/markdown',
-    license='GPL-3',
+    long_description_content_type="text/markdown",
+    license="GPL-3",
     keywords="stow",
     python_requires=">=3.6.*",
-    packages=['pystow'],
+    packages=["pystow"],
     include_package_data=True,
     zip_safe=False,
     entry_points={
-        'console_scripts': [
-            'pystow = pystow.app:main'
+        "console_scripts": [
+            "pystow = pystow.app:main"
         ],
     },
     classifiers=[
-        'Development Status :: Beta',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Communications :: Email',
-        'Topic :: Software Development :: Bug Tracking',
+        "Development Status :: Beta",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Email",
+        "Topic :: Software Development :: Bug Tracking",
     ]
 )
