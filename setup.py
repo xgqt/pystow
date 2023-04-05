0eb9098c6bc92d74d6bce1dc43b3811a5ccf84c0
Maciej Barć <xgqt@gentoo.org>
2023-04-05 14:03:56 +0200

    setup.py: bump to 1.2.3
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def readme():
 
 setup(
     name="pystow",
-    version="1.2.2",
+    version="1.2.3",
     description="GNU Stow rewritten in Python",
     author="Maciej Barć",
     author_email="xgqt@riseup.net",
